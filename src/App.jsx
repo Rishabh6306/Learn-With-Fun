587951150201752a3e124e28b4142bde184f114d
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-08-29 11:44:44 +0530

    Initial Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className='bg-red-600 text-white text-3xl py-6 px-5'>
+      Hello all setup Completed
+    </h1>
+  )
+}
+
+export default App;
