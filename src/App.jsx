9e2fe3f15903bb0a76f969ca79ff3b449596df93
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-08-30 20:55:47 +0530

    Library Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './App.css'
 import Navbar from './Components/Home/Navbar'
+import Library from './Pages/Library';
 
 function App() {
   return (
+    <>
     <Navbar/>
+    <Library/>
+    </>
   )
 }
 
--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -30,64 +30,72 @@ function Navbar() {
   }, []);
 
   return (
-    <header className=" h-screen">
       <nav className={`bg-gradient-to-b from-green-500 to-cyan-500 p-5 md:flex md:justify-between md:items-center text-white ${isMenuOpen ? 'shadow-md' : ''}`}>
         <div className="flex justify-between items-center">
-          <span className="text-3xl font-[Poppins] font-bold cursor-pointer">            <img
-            className="w-20 inline ml-7 rounded-full"
-            src='./src/assets/LOGO.png'
-            alt="logo"
-          />
+          <span className="text-3xl font-[Poppins] font-bold cursor-pointer">
+            <img
+              className="w-20 inline ml-4 rounded-full"
+              src='./src/assets/LOGO.png'
+              alt="logo"
+            />
           </span>
           <span
             className="text-3xl md:hidden block mx-2 cursor-pointer"
             onClick={toggleMenu}
           >
-            {/* <AiOutlineMenu /> */}
-            {isMenuOpen ? <AiOutlineClose/> : <AiOutlineMenu/>}
+            {isMenuOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
           </span>
         </div>
 
         <ul
-          className={`${isMenuOpen ? 'max-h-[28rem]' : 'max-h-0'
-            } md:flex md:items-center z-[-1] md:z-auto md:static w-full md:w-auto md:py-0 py-2 md:pl-0 pl-7 transition-max-h duration-700 ease-in-out overflow-hidden text-center`}
+          className={`${isMenuOpen ? 'max-h-[34rem]' : 'max-h-0'
+            } md:flex md:items-center z-[-1] md:z-auto md:static w-full md:w-auto md:py-0 py-1 md:pl-0 pl-7 transition-max-h duration-700 ease-in-out overflow-hidden text-center`}
         >
-          <li className="mx-5 my-6 md:my-0">
-            <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
+          <li className="mx-4 my-4 md:my-0">
+          <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
               Home
             </a>
           </li>
-          <li className="mx-5 my-6 md:my-0">
-            <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
+          <li className="mx-4 my-4 md:my-0">
+          <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
               Documentation
             </a>
           </li>
-          <li className="mx-5 my-6 md:my-0">
-            <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
+          <li className="mx-4 my-4 md:my-0">
+          <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
               Features
             </a>
           </li>
-          <li className="mx-5 my-6 md:my-0">
-            <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
+          <li className="mx-4 my-4 md:my-0">
+          <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
               Notes
             </a>
           </li>
-          <li className="mx-5 my-6 md:my-0">
-            <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
+          <li className="mx-4 my-4 md:my-0">
+          <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
               Games
             </a>
           </li>
-          <li className="mx-5 my-6 md:my-0">
-            <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
+          <li className="mx-4 my-4 md:my-0">
+          <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
+              Library
+            </a>
+          </li>
+          <li className="mx-4 my-4 md:my-0">
+          <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
+              Code
+            </a>
+          </li>
+          <li className="mx-4 my-4 md:my-0">
+          <a href="#" className="text-3xl hover:text-indigo-800 hover:border-b-4 border-indigo-600 duration-500">
               Contact Us
             </a>
           </li>
-          <button className="bg-violet-300 hover:bg-vi[#ff4000] active:bg-red-500 ease-in-out text-white text-xl py-3 px-10 mx-8 font-[Poppins] durationindigo-800 hover:border-b-4 border-indigo-600ded-md">
+          <button className="bg-violet-600 hover:bg-violet-500 active:bg-red-500 ease-in-out text-white text-xl py-3 px-10 mx-6 font-[Poppins] duration-700 rounded-lg ">
             Get Started
           </button>
         </ul>
       </nav>
-    </header>
   );
 }
 
