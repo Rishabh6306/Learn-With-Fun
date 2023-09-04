273722ed15044ef0eadd74ad9542637b91a88164
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-09-04 20:46:41 +0530

    Divide the formSubmit Server-Side code in seperated files

--- Server/controller/formSubitController.js
+++ Server/controller/formSubitController.js
@@ -1,1 +1,39 @@
-// const formSubmit = 
+import validator from 'validator';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.GMAIL_ID,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+const formSubmit = async (req, res) => {
+    const { name, email, message } = req.body;
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({ error: 'Invalid email address' });
+    }
+
+    console.log(req.body);
+
+    try {
+        await transporter.sendMail({
+            from: email,
+            to: process.env.GMAIL_ID,
+            subject: 'New Contact Form Submission',
+            html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
+        });
+
+        console.log('Email sent successfully');
+        res.status(200).json({ message: 'Form submitted and email sent successfully' });
+    } catch (error) {
+        console.error('Error sending email:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+export default formSubmit;
--- Server/index.js
+++ Server/index.js
@@ -5,6 +5,9 @@ import cors from 'cors';            // Cross-Origin Resource Sharing middleware
 import connectToDatabase from './db/db.js'; // Function to connect to the database
 import notesRoutes from './routes/NotesRoutes.js'; // Import user-related routes
 
+import formSubitController from './controller/formSubitController.js' // import formSubitController
+
+
 // Load environment variables from a .env file if present
 dotenv.config();
 
@@ -28,7 +31,10 @@ app.use('/', notesRoutes); // GET, POST, and other user-related routes
 app.delete('/:id', notesRoutes); // DELETE user route
 app.put('/:id', notesRoutes); // PUT (update) user route
 
-// Define other route groups and import their route files here if needed
+
+// Form subit routes with controller
+app.post('/api/contact', formSubitController);
+
 
 // Start the Express server and listen on the specified port
 app.listen(port, () => {
