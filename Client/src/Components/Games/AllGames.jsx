7bf132a36134b8fee488e25ed42b7b5f3a02316d
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-09-13 00:34:25 +0530

    Number Guessing Game Completed

--- Client/src/Components/Games/AllGames.jsx
+++ Client/src/Components/Games/AllGames.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 function AllGames() {
   return (
-    <div className='bg-[url(./src/Components/Games/mainBgc.png)] bg-center bg-cover grid grid-cols-2 gap-6 text-center p-10 text-white h-screen overflow-hidden '>
-      <div className="p-4 border-2 border-sky-500 flex flex-col items-center">
-        <h1>Guess Number Game</h1>
+    <div className='bg-[url(./src/Components/Games/mainBgc.png)] bg-center bg-cover grid grid-cols-1 md:grid-cols-2 gap-6 text-center p-3 md:p-10 text-white overflow-hidden '>
+      <div className="p-5 border-2 border-sky-500 flex flex-col items-center rounded-lg">
+        <h1 className='text-3xl md:text-4xl font-bold mb-3'>Guess Number Game</h1>
         <p>In this game, you must guess a random number between a given range. To play, you first choose a range, such as 1 to 100. Then, you enter your guess in an input box. The game will then tell you if your guess is too high or too low. You continue guessing until you guess the correct number. <br />  This is a fun and engaging game that tests your logic and guessing skills.</p>
-        <Link to="/guessGame" className='text-3xl text-white my-5 rounded-3xl py-3 px-5 bg-orange-400'>Play</Link>
+        <Link to="/guessGame" className='text-3xl text-white my-5 rounded-3xl py-3 px-10 bg-orange-400'>Play</Link>
       </div>
 
       <div className="p-4 border-2 border-sky-500  ">
--- Client/src/Components/Games/NumberGuessing/NumberGuessingGame.jsx
+++ Client/src/Components/Games/NumberGuessing/NumberGuessingGame.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { GiFastBackwardButton } from 'react-icons/gi'
 
 function NumberGuessingGame() {
   const [targetNumber, setTargetNumber] = useState(generateRandomNumber());
@@ -48,43 +49,58 @@ function NumberGuessingGame() {
     setCurrentGuess("");
   }
 
-  return (
-    <div className='h-[80vh] flex items-center justify-center bg-[url(./src/Components/Games/NumberGuessing/bgc.jpg)] bg-center bg-cover'>
-      <div className="md:mx-auto p-4 mx-4 sm:p-6 bg-white rounded-md shadow-md w-96">
-        <h1 className="text-3xl font-semibold mb-4 text-center">Number Guessing Game</h1>
-        <p>Guess a number between 1 and 100:</p>
-        <input
-          type="number"
-          value={currentGuess}
-          onChange={(e) => setCurrentGuess(e.target.value)}
-          className="border-2 border-gray-400 outline-none w-full ssm:w-4/6 rounded-md px-2 py-1 mt-2"
-          min="1"
-          max="100"
-        />
-        <button onClick={handleGuess} className="bg-blue-500 text-white px-4 py-2 m-3 rounded-md">
-          Guess
-        </button>
-        {message && (
-          <div className="mt-4 text-center break-words text-xl font-semibold">
-            {message}
-          </div>
-        )}
-        <div className="mt-4">
-        <div className="bg-gray-300 h-4 rounded-md">
-            <div
-              className={`h-full bg-gradient-to-tr from-green-500 to-violet-500 duration-500`}
-              style={{ width: `${calculateProgress()}%` }}
-            ></div>
-          </div>
-        </div>
+  function handleKeyPress(event) {
+    if (event.key === "Enter") {
+      handleGuess();
+    }
+  }
 
-        <div className="mt-4 max-h-36 overflow-x-scroll">
-          <h3 className="text-center">Your Previous Guesses:</h3>
-          <ul>
-            {guesses.map((guess, index) => (
-              <li key={index}>{guess}</li>
-            ))}
-          </ul>
+
+  function goBack() {
+    window.history.back(); // This line takes the user back to the previous page
+  }
+
+  return (
+    <div className="h-screen bg-[url(./src/Components/Games/NumberGuessing/bgc.jpg)] bg-center bg-cover">
+      <button onClick={goBack} className="bg-blue-800 text-white px-4 py-2 m-3 rounded-md"><GiFastBackwardButton /></button>
+      <div className='flex mt-10 sm:mt-24 items-center justify-center'>
+        <div className="md:mx-auto p-4 mx-4 sm:p-6 bg-white rounded-md shadow-md w-96">
+          <h1 className="text-3xl font-semibold mb-4 text-center">Number Guessing Game</h1>
+          <p>Guess a number between 1 and 100:</p>
+          <input
+            type="number"
+            value={currentGuess}
+            onKeyPress={handleKeyPress}
+            onChange={(e) => setCurrentGuess(e.target.value)}
+            className="border-2 border-gray-400 outline-none w-full ssm:w-4/6 rounded-md px-2 py-1 mt-2"
+            min="1"
+            max="100"
+          />
+          <button onClick={handleGuess} className="bg-blue-500 text-white px-4 py-2 m-3 rounded-md">
+            Guess
+          </button>
+          {message && (
+            <div className="mt-4 text-center break-words text-xl font-semibold">
+              {message}
+            </div>
+          )}
+          <div className="mt-4">
+            <div className="bg-gray-300 h-4 rounded-md">
+              <div
+                className={`h-full bg-gradient-to-tr from-green-500 to-violet-500 duration-500`}
+                style={{ width: `${calculateProgress()}%` }}
+              ></div>
+            </div>
+          </div>
+
+          <div className="mt-4 max-h-36 overflow-x-scroll">
+            <h3 className="text-center">Your Previous Guesses:</h3>
+            <ul>
+              {guesses.map((guess, index) => (
+                <li key={index}>{guess}</li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
