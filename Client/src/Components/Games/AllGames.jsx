079288f86d061c2cd5346dad612cae379937ea27
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-09-13 23:10:27 +0530

    Optimize the NumberGussingGame code

--- Client/src/Components/Games/AllGames.jsx
+++ Client/src/Components/Games/AllGames.jsx
@@ -12,7 +12,7 @@ function AllGames() {
 
       <div className="p-5 border-2 border-sky-500 flex flex-col items-center rounded-lg">
         <h1 className='text-3xl md:text-4xl font-bold mb-3'>Speed Typing Game</h1>
-        <p>In this game, you must guess a random number between a given range. To play, you first choose a range, such as 1 to 100. Then, you enter your guess in an input box. The game will then tell you if your guess is too high or too low. You continue guessing until you guess the correct number. <br />  This is a fun and engaging game that tests your logic and guessing skills.</p>
+        <p>A typing game in which players must type a paragraph of text as quickly as possible. The game keeps track of typing speed and accuracy, and players can compare their scores to other players. They also can check that how many words they can type in a minutes. <br /> Now Improve your typing speed and accuracy with this fun and addictive game! </p>
         <Link to="/typingGame" className='text-3xl text-white my-5 rounded-3xl py-3 px-10 bg-orange-400'>Play</Link>
       </div>
 
